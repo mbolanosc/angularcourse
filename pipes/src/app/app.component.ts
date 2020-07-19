a2f2820b741fc8d0343b7af512e78f09b114a9d6
mbolanosc <mrybc96@gmail.com>
2020-07-19 17:24:55 -0600

    some pipes from angular

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+ // title = 'pipes';
+
+  name:string = "Spiderman"
+
+  array = [1,2,3,4,5,6,7,8,9,10] 
+
+  PI:number = Math.PI;
+
+  percent: number = 0.2435;
+
+  salary:number = 1234.5;
+
+  hero={
+    name: "peter parker",
+    keyname: "spiderman",
+    age: 28,
+      address:{
+        city:"NY",
+        nbh: "Queens"
+      }
+  }
+
 }
+
