d10d86334629db4f4599cdbba0b1b475c44bfafb
mbolanosc <mrybc96@gmail.com>
2020-08-17 19:57:46 -0600

    navbar

--- spotiapp/src/app/app-routing.module.ts
+++ spotiapp/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'home', component: HomeComponent },
+  { path : 'search', component: SearchComponent },
+  { path : '', pathMatch: 'full', component: HomeComponent },
+  { path : '**', pathMatch: 'full', component: HomeComponent }
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
